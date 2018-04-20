856af4dd4e50a9e67703c4f4d8929c92dade5411
Mchakka <mchakka@gmail.com>
2018-04-19 23:35:03 -0400

    Breakout Scene First Commit

--- src/cs1302/arcade/BreakoutScene.java
+++ src/cs1302/arcade/BreakoutScene.java
@@ -1,20 +1,27 @@
 package cs1302.arcade;
 
+import java.awt.event.KeyEvent;
+
 import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
 import javafx.stage.Stage;
 
 public class BreakoutScene {
-	
+
 	
 	
 	
 	public static Scene createBreakoutScene(Stage stage, Scene mainScene) {
-		BorderPane bp = new BorderPane();
-		bp.setPadding(new Insets(40));
-				
+		Group group = new Group();           // main container
+		Scene scene = new Scene(group, 600, 600);
 
 		Button backButton = new Button();
 		backButton.setText("Back");
@@ -23,12 +30,50 @@ public class BreakoutScene {
 			stage.sizeToScene();
 		});
 		
-		bp.setTop(backButton);
-		
-		Scene breakoutScene = new Scene(bp);
+		GridPane gPane = new GridPane();
 		
 		
-		return breakoutScene;
+		
+		
+		Rectangle rectangle = new Rectangle();
+		rectangle.setX(250);
+		rectangle.setY(580);
+		group.getChildren().add(rectangle); 
+		
+		Circle ball = new Circle();
+		ball.setCenterX(300);
+		ball.setCenterY(570);
+		ball.setRadius(10);
+		group.getChildren().add(ball); 
+		boolean ready = false;
+		
+		group.setOnKeyPressed(event -> {
+			System.out.println(event);
+			if (event.getCode() == KeyCode.LEFT && rectangle.getX() >= 10)
+				{
+					rectangle.setX(rectangle.getX() - 10.0);
+					ball.setCenterX(ball.getCenterX() - 10.0);
+				}
+			if (event.getCode() == KeyCode.RIGHT && rectangle.getX() <= 490)
+				{
+					rectangle.setX(rectangle.getX() + 10.0);
+					ball.setCenterX(ball.getCenterX() + 10.0);
+				}
+			if (event.getCode() == KeyCode.RIGHT)
+				{
+					//hh
+				}
+		    });
+		
+		rectangle.setWidth(100);
+		rectangle.setHeight(10);
+		
+		ball.setFill(Color.RED);
+		rectangle.setFill(Color.BLACK);
+		  
+		
+		
+		group.requestFocus();
+		return scene;
 	}
-
 }
